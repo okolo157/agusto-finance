ba582961152f7f186d0dda55f755dc0d3b88829a
vik1234-del <victor.okolo@pau.edu.ng>
2025-02-05 19:40:31 +0100

    persist white color for header text

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-[80vh] p-4 gap-6 md:gap-8 lg:gap-10">
-      <p className="font-bold text-4xl md:text-3xl lg:text-4xl text-center">
+      <p className="font-bold text-4xl md:text-3xl lg:text-4xl text-center text-white">
         Track your finances with ease.
       </p>
       <Link
